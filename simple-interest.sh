05ddc3ac19013ba41edfea53f416e113a35037b2
VIDIT2101 <119759440+VIDIT2101@users.noreply.github.com>
2025-01-01 15:30:35 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# A simple script to calculate simple interest
+
+echo "Enter Principal:"
+read principal
+echo "Enter Rate of Interest:"
+read rate
+echo "Enter Time (in years):"
+read time
+
+interest=$(echo "$principal * $rate * $time / 100" | bc -l)
+echo "Simple Interest: $interest"
